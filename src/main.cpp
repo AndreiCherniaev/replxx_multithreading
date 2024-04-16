5bd2892eac59745dfa22031df7990ae472d541d3
AndreiCherniaev <glscene2010@gmail.com>
2024-04-17 01:59:07 +0900

    remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -27,29 +27,3 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-
-
-void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-{
-    QByteArray localMsg= msg.toLocal8Bit();
-    const char *file= context.file ? context.file : "";
-    const char *function= context.function ? context.function : "";
-
-    switch(type){
-    case QtDebugMsg:
-        fprintf(stderr, "DebugUsuall: %s\n", localMsg.constData());
-        break;
-    case QtInfoMsg:
-        fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
-        break;
-    case QtWarningMsg:
-        fprintf(stderr, "Warning: %s\n", localMsg.constData());
-        break;
-    case QtCriticalMsg:
-        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
-        break;
-    case QtFatalMsg:
-        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
-        break;
-    }
-}
